e9ce25ae98eb7d8c0fbdd9f61282ae76200bf9c2
qingcaier <1721119157@qq.com>
2025-08-29 22:17:10 +0800

    修改数字转换字符串方法

--- Assets/Scripts/BasicFunction/NumberController.cs
+++ Assets/Scripts/BasicFunction/NumberController.cs
@@ -10,20 +10,52 @@ public class NumberController : MonoBehaviour
         instance = this;
     }
 
-    //将数字转化为字符串（万、亿）
+    //将数字转化为字符串（万、亿），保留千位一下的数字，如101000转化为10万1000
     public string NumberToChinaString(int number)
     {
-        if(number >= 100000000)
+        string str;
+        if (number >= 100000000)
         {
-            return (number / 100000000).ToString()+"亿";
-        }else if (number >= 10000)
+            int yi = number / 100000000;
+            int wan = number % 100000000 / 10000;
+            int qian = number % 10000;
+
+            if (wan == 0 && qian == 0)
+            {
+                str = $"{yi}亿";
+            }
+            else if (wan != 0 && qian == 0)
+            {
+                str = $"{yi}亿{wan}万";
+            }
+            else if (wan == 0 && qian != 0)
+            {
+                str = $"{yi}亿 {qian}";
+            }
+            else
+            {
+                str = $"{yi}亿{wan}万 {qian}";
+            }
+        }
+        else if (number >= 10000)
         {
-            return (number / 10000).ToString()+"万";
+            int wan = number / 10000;
+            int qian = number % 10000;
+
+            if (qian == 0)
+            {
+                str = $"{wan}万";
+            }
+            else
+            {
+                str = $"{wan}万 {qian}";
+            }
         }
         else
         {
-            return number.ToString();
+            str = $"{number}";
         }
-    }
 
+        return str;
+    }
 }
